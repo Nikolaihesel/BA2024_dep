bd5dbbe32f885576dd98b9d7a963954c08e64d2a
Nikolaihesel <Nikolaihesel@icloud.com>
2024-12-30 19:54:55 +0100

    cors prod 4
    Co-Authored-By: Nicole le Fevre <83703145+nicolelefevre98@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,7 @@ const allowedOrigins = ['http://localhost:5173', 'https://ba2024.onrender.com'];
 
 app.use(
 	cors({
-		origin: (origin, callback) => {
-			if (!origin || allowedOrigins.includes(origin)) {
-				callback(null, true);
-			} else {
-				callback(new Error('Not allowed by CORS'));
-			}
-		},
+		origin: allowedOrigins,
 		methods: ['GET', 'POST', 'PUT', 'DELETE'],
 		credentials: true,
 	})
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,6 +20,7 @@ const Login = () => {
 					method: 'POST',
 					headers: {
 						'Content-Type': 'application/json',
+						credentials: 'include',
 					},
 					body: JSON.stringify({ username, password }),
 				}
